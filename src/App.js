bf34b1ff927e13887024e1fdcca545a59afeda81
Nguyen Tan Tai <nguyentantai9a3@gmail.com>
2023-03-24 11:53:43 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import CryptoJS from "crypto-js";
 
 function App() {
+  const _handleSubmit = async () => {
+    try {
+      const reqTime = new Date().getTime();
+
+      const listBank = await axios.post(
+        "https://sbgateway.zalopay.vn/api/getlistmerchantbanks",
+        {
+          appid: 2554,
+          reqtime: reqTime,
+          mac: CryptoJS.HmacSHA256(
+            `2554|${reqTime}`,
+            "sdngKKJmqEMzvh5QQcdD2A9XBSKUNaYn"
+          ),
+        },
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
+
+      console.log(listBank);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +38,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={_handleSubmit}>Test</button>
         <a
           className="App-link"
           href="https://reactjs.org"
